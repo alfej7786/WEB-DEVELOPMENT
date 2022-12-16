b236605d12a2388ab7434264d69a8498df668e6b
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2022-12-16 16:46:51 -0500

    update

--- 6. JQUERY/index.js
+++ 6. JQUERY/index.js
@@ -1,4 +1,60 @@
 // $("h1").css("color", "red");
 // $("h1").addClass("big-title margin");
-$("h1").text("Bye");
-$("button").html("Heollo")
+// $("h1").text("Bye");
+// $("button").html("Heollo")
+// console.log($("img").attr("src"));
+// console.log($("a").attr("href", "http://www.yahoo.com"));
+
+
+// $("h1").before("<button>New</button>")
+// $("h1").after("<button>After</button>")
+
+
+// EVENT LISTNERS
+// $("button").click(function() {
+//     $("h1").css("color", "red")
+// });
+
+// $("input").keypress(function(event) {
+//     console.log(event.key);
+// })
+
+// $("input").keypress(function(event) {
+//     $("h1").text(event.key);
+// });
+
+
+// $("h1").on("mouseover",function(event) {
+//     $("h1").css("color", "purple");
+// });
+
+
+// $("h1").click("mouseover",function(event) {
+//     $("h1").css("color", "purple");
+// });
+
+
+
+// HIDE SHOW (ANIMATION)
+// $("button").on("click",function() {$("h1").hide();
+// });
+
+// $("button").on("click",function() {$("h1").toggle();
+// });
+
+// $("button").on("click",function() {$("h1").fadeToggle();
+// });
+
+
+// $("button").on("click",function() {$("h1").slideToggle();
+// });
+
+// $("button").on("click",function() {$("h1").animate({opacity:0.5});
+// });
+
+// $("button").on("click",function() {$("h1").animate({margin: 50});
+// });
+
+// $("button").on("click",function() {
+//     $("h1").slideUp().slideDown().animate({opacity:0.5});
+// });
