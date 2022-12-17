3186db2d6f9ca7c3a96f5c1aa07b799bc18d0c30
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2022-12-17 00:42:27 -0500

    update

--- 7. BACKEND PROGRAMMING/1. NODE JS/index.js
+++ 7. BACKEND PROGRAMMING/1. NODE JS/index.js
@@ -1,5 +1,18 @@
-// jshint esversion:6
-const fs = require("fs");
-// Copy File.txt to File2.txt
-fs.copyFileSync("file.txt", "file2.txt");
+// // jshint esversion:6
+// const fs = require("fs");
+// // Copy File.txt to File2.txt
+// fs.copyFileSync("file.txt", "file2.txt");
 
+
+// SUPERHEROES
+// var superheroes = require("superheroes");
+// var mySuperhero = superheroes.random();
+
+// console.log(mySuperhero);
+
+
+// SUPERVILLAINS
+var supervillains = require("supervillains");
+var mySupervillain = supervillains.random();
+
+console.log(mySupervillain);
