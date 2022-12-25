ddbe25f605279f112a9c238b112c6857a09f3226
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2022-12-25 00:35:49 -0500

    ToDo project in progress

--- 7. BACKEND PROGRAMMING/4. EJS/ToDoList-V1/app.js
+++ 7. BACKEND PROGRAMMING/4. EJS/ToDoList-V1/app.js
@@ -2,41 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+var items = ["Buy Food", "Cook Food","Eat Food"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({encoded : true}));
 
 app.get("/", function(req, res){
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error! Current Day is equal to: " + currentDay);
-    }
+    var day = today.toLocaleDateString("en-US", options)
     
-    res.render("list", {kindOfDay: day});
+    res.render("list", {kindOfDay: day, newListItems: items});
 });
+
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+})
 
 
 app.listen(8080, function(){
