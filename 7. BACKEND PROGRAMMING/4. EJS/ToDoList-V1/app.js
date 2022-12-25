082447918122750fc3f895578947f47cae94f68c
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2022-12-25 00:00:59 -0500

    Working on a ToDo project

--- 7. BACKEND PROGRAMMING/4. EJS/ToDoList-V1/app.js
+++ 7. BACKEND PROGRAMMING/4. EJS/ToDoList-V1/app.js
@@ -2,8 +2,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
-    res.send("Hello World!");
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error! Current Day is equal to: " + currentDay);
+    }
+    
+    res.render("list", {kindOfDay: day});
 });
 
 
