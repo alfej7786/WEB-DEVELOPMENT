a4f174b917fbb8291d3027d4898a52d9a6007074
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2022-12-28 22:59:29 -0500

    finished Blog Website project

--- CHALLENGE 3 - BLOG WEBSITE/ejs-challenge/app.js
+++ CHALLENGE 3 - BLOG WEBSITE/ejs-challenge/app.js
@@ -11,67 +11,60 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-let gblData = [];
-
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-// HOME
-app.get("/", function(req, res){
+let posts = [];
+
+app.get("/", function (req, res) {
   res.render("home", {
-    startingContent: homeStartingContent, 
-    gblData: gblData
+    startingContent: homeStartingContent,
+    posts: posts
   });
 });
 
-// ABOUT
-app.get("/about",  function(req, res){
-  res.render("about", {abtContent: aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
 
-// CONTACT
-app.get("/contact", function(req,res) {
-  res.render("contact", {cntContent: contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
 });
 
-// COMPOSE
-app.get("/compose", function(req, res) {
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function(req, res) {
-  const data = {
-    TITLE: req.body.titlePublish,
-    POST: req.body.postBody
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
   };
-  gblData.push(data);
+
+  posts.push(post);
+
   res.redirect("/");
+
 });
 
-//
-app.get('/posts/:postName', function(req, res) {
+app.get("/posts/:postName", function (req, res) {
   const requestedTitle = _.lowerCase(req.params.postName);
 
-  // forEach loop
-  gblData.forEach(function(data) {
-    const storedTitle = _.lowerCase(data.TITLE);
-  
-    if(requestedTitle === storedTitle) {
-      console.log("Match Found!");
-    } else {
-      console.log("No Match Found!");
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
     }
   });
+
 });
 
-
-
-
-
-
-
-app.listen(8080, function() {
-  console.log("Server started on port 8080");
+app.listen(8080, function () {
+  console.log("Server started on port 3000");
 });
