54e99e2776e2ff5e737083792fec0d042463b5aa
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-01-05 01:19:27 -0500

    working on a ToDoList-V2

--- 8. DATABASES/ToDoList-V2/app.js
+++ 8. DATABASES/ToDoList-V2/app.js
@@ -2,51 +2,96 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const date = require(__dirname + "/date.js");
 
 const app = express();
 
-
-
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({encoded : true}));
+app.use(bodyParser.urlencoded({ encoded: true }));
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB', { useNewUrlParser: true });
 
-    var day = today.toLocaleDateString("en-US", options)
-    
-    res.render("list", {listTitle: day, newListItems: items});
+const itemSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+    name: "Welcome to your ToDoList "
 });
 
-app.post("/", function(req, res){
-    var item = req.body.newItem;
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/work");
-    } else {
-        items.push(item);
-        res.redirect("/");
-    }
-    
+const item2 = new Item({
+    name: "Hit the + button to off a new Item "
 });
 
-app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work List", newListItems: workItems});
+const item3 = new Item({
+    name: "<-- hit this to delete an item"
 });
 
-app.get("/about", function(req, res){
+const defaultItems = [item1, item2, item3];
+
+
+
+
+app.get("/", function (req, res) {
+
+
+
+    Item.find({}, function (err, foundItems) {
+
+        if (foundItems.length === 0) {
+            Item.insertMany(defaultItems, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Successfully saved default items to database.")
+                }
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", { listTitle: "Today", newListItems: foundItems });
+        }
+
+    });
+
+});
+
+app.post("/", function (req, res) {
+    const itemName = req.body.newItem;
+
+    const item = new Item({
+        name: itemName
+    });
+    item.save();
+
+    res.redirect("/");
+
+});
+
+app.post("/delete", function(req, res) {
+    const checkedItemID = req.body.checkbox;
+
+    Item.findByIdAndRemove(checkedItemID, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log("Successfully deleted");
+            res.redirect("/");
+        }
+    });
+});
+
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.get("/about", function (req, res) {
     res.render("about");
 });
 
-app.listen(8080, function(){
+app.listen(8080, function () {
     console.log('listening on port 8080');
 });
