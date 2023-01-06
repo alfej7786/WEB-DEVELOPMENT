d60547764a91e7d7f650e6eed3dd385207b3e53e
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-01-05 23:52:30 -0500

    Blog website - V2 finished

--- CHALLENGE 4 - BLOG WEBSITE UPGRADE/Blog-with-Database-Starting-Files/app.js
+++ CHALLENGE 4 - BLOG WEBSITE UPGRADE/Blog-with-Database-Starting-Files/app.js
@@ -75,6 +75,6 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(8080, function() {
+  console.log("Server started on port 8080");
 });
