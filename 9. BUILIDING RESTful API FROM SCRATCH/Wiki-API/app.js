0075dc42c49127e2fe6d5465e4380642e87f825c
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-01-10 15:32:11 -0500

    database created

--- 9. BUILIDING RESTful API FROM SCRATCH/Wiki-API/app.js
+++ 9. BUILIDING RESTful API FROM SCRATCH/Wiki-API/app.js
@@ -15,8 +15,14 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 //TODO
-mongoose.connect('mongodb://127.0.0.1:27017/wikiDB', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/wikiDB', { useNewUrlParser: true });
 
+const articleSchema = {
+    title: String,
+    content: String
+};
+
+const Article = mongoose.model("Article", articleSchema);
 
 app.listen(8080, function() {
   console.log("Server started on port 8080");
