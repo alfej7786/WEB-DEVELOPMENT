c131bb1d242d8cd3cee55bcfafb466647a64b1e0
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-02-01 23:46:26 -0500

    authentication

--- 10. AUTHENTICATION AND SECURITY-1/Secrets - Starting Code/app.js
+++ 10. AUTHENTICATION AND SECURITY-1/Secrets - Starting Code/app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 
 const app = express();
@@ -9,6 +10,15 @@ const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true });
+
+const userSchema = {
+  email: String,
+  password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
   res.render("home");
@@ -21,6 +31,13 @@ app.get("/login",  function(req, res){
 app.get("/rigister", function(req,res) {
     res.render("rigister");
 });
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+})
 
 
 app.listen(8080, function(){
