c5ede0347eeacea63ecdaaf13ac0d99f448e917f
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-02-02 00:22:16 -0500

    commit

--- 10. AUTHENTICATION AND SECURITY-1/Secrets - Starting Code/app.js
+++ 10. AUTHENTICATION AND SECURITY-1/Secrets - Starting Code/app.js
@@ -7,6 +7,7 @@ const encrypt = require('mongoose-encryption');
 const ejs = require('ejs');
 
 const app = express();
+console.log(process.env)
 
 
 app.use(express.static("public"));
@@ -20,8 +21,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, {secret : secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
