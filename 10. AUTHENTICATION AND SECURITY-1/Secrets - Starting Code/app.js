452784e5e26fffd110f5a5ddd14f099ce94ab553
Alfej Savaya <105317617+alfej7786@users.noreply.github.com>
2023-02-02 00:06:53 -0500

    security

--- 10. AUTHENTICATION AND SECURITY-1/Secrets - Starting Code/app.js
+++ 10. AUTHENTICATION AND SECURITY-1/Secrets - Starting Code/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 const ejs = require('ejs');
 
 const app = express();
@@ -13,10 +14,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect('mongodb://127.0.0.1:27017/userDB', { useNewUrlParser: true });
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
-};
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret : secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
